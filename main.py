eb0c7e38327aff18b0948735711a183dd52aa483
Ziscool <Ziscool@mail.ru>
2022-01-07 14:56:56 +0500

    Изменение спрайта поля, сокращение до 14х14(было 16х16), класс обрезки спрайтов, увеличение окна, добавление нижней панели

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import os
 
 GRASS = 'grass.png'
 BOX = 'box.png'
+GRASS_SP = pygame.sprite.Group()
+BOX_SP = pygame.sprite.Group()
 DICT_OF_SP = {0: GRASS,
               1: BOX}
-
+BAKE_SP = pygame.sprite.Group()
 
 
 def load_image(name, colorkey=None):
@@ -28,7 +30,7 @@ class Board:
         self.board = [[0] * width for _ in range(height)]
         self.left = 10
         self.top = 10
-        self.cell_size = 30
+        self.cell_size = 34
 
     def render(self, screen, *groups):
         for y in range(self.height):
@@ -44,6 +46,10 @@ class Board:
                 pygame.draw.rect(screen, pygame.Color('white'), (
                     x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
                     self.cell_size), 1)
+        total = 10
+        for i in range(7):
+            pygame.draw.rect(screen, pygame.Color('white'), (total, 485, 67, 67), 2)
+            total += 68
 
     def set_view(self, left, top, cell_size):
         self.left = left
@@ -68,10 +74,50 @@ class Board:
             print(cell)
 
 
+class CutPict(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(BAKE_SP)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+        self.image = pygame.transform.scale(self.image, (80, 80))
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns, sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(frame_location, self.rect.size)))
+                return self.frames
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+        self.image = pygame.transform.scale(self.image, (60, 60))
+
+
+class Bake(pygame.sprite.Sprite):
+    image = load_image('mcblocks.png')
+
+    def __init__(self, frames, x, y):
+        super().__init__(BAKE_SP)
+        self.image = Bake.image
+        self.frames = frames
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self, *args):
+        self.image = self.frames[60]
+        self.image = pygame.transform.scale(self.image, (60, 60))
+
+
 class Box(pygame.sprite.Sprite):
     image = load_image('box.png')
 
-    def __init__(self, x, y, a, gp_sp, *group):
+    def __init__(self, x, y, a, *group):
         super().__init__(*group)
         self.image = Box.image
         self.rect = self.image.get_rect()
@@ -101,23 +147,16 @@ class Grass(pygame.sprite.Sprite):
 
 def main():
     pygame.init()
-    size = 500, 500
+    size = 500, 560
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Координаты клетки')
-    board = Board(16, 16)
+    board = Board(14, 14)
+    clock = pygame.time.Clock()
 
-    grass_sp = pygame.sprite.Group()
-    grass = pygame.sprite.Sprite()
-    grass.image = load_image(GRASS)
-    grass_sp.add(grass)
+    dragon = CutPict(load_image("mcblocks.png"), 18, 36, 5, 480)
+    # Bake(dragon, 68, 485)
 
-    box_sp = pygame.sprite.Group()
-    box = pygame.sprite.Sprite()
-    box.image = load_image(BOX)
-    box_sp.add(BOX)
-
-
-running = True
+    running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -125,10 +164,13 @@ running = True
             if event.type == pygame.MOUSEBUTTONDOWN:
                 board.get_click(event.pos)
         screen.fill((0, 0, 0))
-        grass_sp.draw(screen)
-        box_sp.draw(screen)
-        board.render(screen, box_sp, grass_sp)
+        BAKE_SP.draw(screen)
+        BAKE_SP.update()
+        GRASS_SP.draw(screen)
+        BOX_SP.draw(screen)
+        board.render(screen, BOX_SP, GRASS_SP)
         pygame.display.flip()
+        clock.tick(50)
     pygame.quit()
 
 
