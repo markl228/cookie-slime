04135c391c96223429791ce55c6eadfbdfce2c16
Ziscool <Ziscool@mail.ru>
2022-01-10 13:33:13 +0500

    переключение на панели, изменение on_click, get_click, get_cell

--- main.py
+++ main.py
@@ -2,18 +2,15 @@ import pygame
 import sys
 import os
 
-
-GRASS = 'grass.png'
-BOX = 'box.png'
 GRASS_SP = pygame.sprite.Group()
 BOX_SP = pygame.sprite.Group()
 CUT_SP = pygame.sprite.Group()
-DICT_OF_SP = {0: GRASS,
-              1: BOX}
-BAKE_SP = pygame.sprite.Group()
-DICT_POS = {(0, 14):'Bake', (1, 14):'Bake', (0, 15):'Bake', (1, 15):'Bake',
-            (2, 14):'Сonveyor', (3, 14):'Сonveyor', (2, 15):'Сonveyor', (3, 15):'Сonveyor',
-            (4, 14):'Chest', (5, 14):'Chest', (4, 15):'Chest', (5, 15):'Chest'}
+CONVEYOR_SP = pygame.sprite.Group()
+
+INTETF_POS = {(0, 14): 'Bake', (1, 14): 'Bake', (0, 15): 'Bake', (1, 15): 'Bake',
+              (2, 14): 'Conveyor', (3, 14): 'Conveyor', (2, 15): 'Conveyor', (3, 15): 'Conveyor',
+              (4, 14): 'Chest', (5, 14): 'Chest', (4, 15): 'Chest', (5, 15): 'Chest'}
+OBJ_NUM = {'Bake': 1, 'Conveyor': 2, 'Chest': 3}
 
 
 def load_image(name, colorkey=None):
@@ -43,9 +40,12 @@ class Board:
                 if self.board[y][x] == 0:
                     Grass(x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size, groups[1],
                           groups[self.board[y][x]])
-                else:
+                elif self.board[y][x] == 1:
                     Box(x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size, groups[0],
                         groups[self.board[y][x]])
+                elif self.board[y][x] == 2:
+                    Conveyor(x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size, groups[2],
+                             groups[self.board[y][x]])
 
                 pygame.draw.rect(screen, pygame.Color('white'), (
                     x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
@@ -60,25 +60,32 @@ class Board:
         self.top = top
         self.cell_size = cell_size
 
-    def on_click(self, cell):
+    def on_click(self, mouse, cell):
         if not cell[1] >= 14:
-            self.board[cell[1]][cell[0]] = (self.board[cell[1]][cell[0]] + 1) % 2
+            if self.board[cell[1]][cell[0]] == mouse:
+                self.board[cell[1]][cell[0]] = 0
+            else:
+                self.board[cell[1]][cell[0]] = mouse
 
-    def get_cell(self, mouse_pos):
+    def get_cell(self, mouse, mouse_pos):
         cell_x = (mouse_pos[0] - self.left) // self.cell_size
         cell_y = (mouse_pos[1] - self.top) // self.cell_size
         if cell_y >= 14:
-            print(DICT_POS[cell_x, cell_y])
+            mouse = OBJ_NUM[INTETF_POS[cell_x, cell_y]]
+            return mouse
         elif cell_x < 0 or cell_x >= self.width or cell_y < 0 or cell_y >= self.height:
             return None
         return cell_x, cell_y
 
-    def get_click(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        if cell:
-            self.on_click(cell)
+    def get_click(self, mouse, mouse_pos):
+        cell = self.get_cell(mouse, mouse_pos)
+        if cell.__class__.__name__ == 'int':
+            return cell
+        elif cell:
+            self.on_click(mouse, cell)
         else:
             print(cell)
+        return mouse
 
 
 class CutPict(pygame.sprite.Sprite):
@@ -99,12 +106,11 @@ class CutPict(pygame.sprite.Sprite):
                 self.frames.append(sheet.subsurface(pygame.Rect(frame_location, self.rect.size)))
 
     def update(self):
-        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
         self.image = self.frames[324]
         self.image = pygame.transform.scale(self.image, (80, 80))
 
 
-class СonveyorMenu(CutPict):
+class ConveyorMenu(CutPict):
     def __init__(self, sheet, columns, rows, x, y):
         super().__init__(sheet, columns, rows, x, y)
         self.frames = []
@@ -134,12 +140,12 @@ class ChestMenu(CutPict):
         self.image = pygame.transform.scale(self.image, (80, 80))
 
 
-class Сonveyor(pygame.sprite.Sprite):
-    image = load_image('Сonveyor.png')
+class Conveyor(pygame.sprite.Sprite):
+    image = load_image('Conveyor.png')
 
     def __init__(self, x, y, a, *group):
         super().__init__(*group)
-        self.image = Grass.image
+        self.image = Conveyor.image
         self.rect = self.image.get_rect()
         self.image = pygame.transform.scale(self.image, (a, a))
         self.rect.x = x
@@ -171,7 +177,6 @@ class Grass(pygame.sprite.Sprite):
         self.rect.y = y
 
 
-
 def main():
     pygame.init()
     size = 500, 560
@@ -179,9 +184,10 @@ def main():
     pygame.display.set_caption('Координаты клетки')
     board = Board(14, 14)
     clock = pygame.time.Clock()
+    mouse = 1
 
     CutPict(load_image("mcblocks.png"), 18, 36, 5, 480)
-    СonveyorMenu(load_image("mcblocks.png"), 18, 36, 72, 480)
+    ConveyorMenu(load_image("mcblocks.png"), 18, 36, 72, 480)
     ChestMenu(load_image("mcblocks.png"), 18, 36, 140, 480)
 
     running = True
@@ -190,15 +196,15 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                board.get_click(event.pos)
+                mouse = board.get_click(mouse, event.pos)
+                print(mouse)
         screen.fill((0, 0, 0))
         CUT_SP.draw(screen)
         CUT_SP.update()
-        BAKE_SP.draw(screen)
-        BAKE_SP.update()
+        CONVEYOR_SP.draw(screen)
         GRASS_SP.draw(screen)
         BOX_SP.draw(screen)
-        board.render(screen, BOX_SP, GRASS_SP)
+        board.render(screen, BOX_SP, GRASS_SP, CONVEYOR_SP)
         pygame.display.flip()
         clock.tick(50)
     pygame.quit()
